76d1b55cf1e51d3674ac2af96f5f1f69f3724954
dunba <dunbatehinse@yahoo.com>
2021-06-27 23:36:08 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav><div><h3>DUNBA</h3>portfolio</div>    <div><ul>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Connect</li>
+      </ul></div>  </nav>
+
+      <main>
+        <section>        <h4>Hello, I’m Dunba.
+Front End Developer,
+Media Professional.
+</h4>
+<p>            I'm Dunba, a creative storyteller and front end developer with a
+            professional background in news and sports media.I am passionate about media and technology, and i aim to build
+            innovative products at the intersection of both industries.</p></section>
+
+
+
+
+            <section>  
+My Skills
+Through years of experience i have a combination of skills in web development and media production.
+
+Web Design
+<ul>
+  <li>Javascript ES6</li>
+  <li>HTML & CSS</li>
+  <li>SASS</li>
+  <li>React</li>
+  <li>Firebase</li>
+  <li>Material UI</li>
+  <li>Framer Motion</li>
+</ul>
+
+Video Editing
+<ul>
+  <li>Adobe After Effects & Premiere</li>
+  <li>DaVinci Resolve</li>
+  <li>Grass Valley Edius-Pro</li>
+  <li></li>
+
+</ul>
+
+
+</section>
+
+<section>
+  <h4>Projects</h4>
+</section>
+          
+      </main>
+
     </div>
   );
 }
